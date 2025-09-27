80b5a4bc9b85917c8070678b262d5a842ccd3006
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-09-28 02:54:34 +0300

    refactor(apphost, api): split configuration extension methods
    Renamed `СonfigurationExtensions` to `ConfigurationManagerExtensions` in `AppHost` for better alignment with `ConfigurationManager`. Extracted the `GetOrThrow` extension for `IConfiguration` into a new `ConfigurationExtensions` class in the `API` project.
    
    These changes improve separation of concerns and maintainability of configuration-related logic without altering functionality. No breaking changes introduced.

--- Wolfix.Server/Wolfix.AppHost/Extensions/ConfigurationManagerExtensions.cs
+++ Wolfix.Server/Wolfix.AppHost/Extensions/ConfigurationManagerExtensions.cs
@@ -2,9 +2,9 @@ using Microsoft.Extensions.Configuration;
 
 namespace Wolfix.AppHost.Extensions;
 
-public static class СonfigurationExtensions
+public static class ConfigurationManagerExtensions
 {
-    public static string GetOrThrow(this IConfiguration configuration, string key)
+    public static string GetOrThrow(this ConfigurationManager configuration, string key)
     {
         return configuration[key] ?? throw new Exception($"Configuration key {key} not found");
     }
