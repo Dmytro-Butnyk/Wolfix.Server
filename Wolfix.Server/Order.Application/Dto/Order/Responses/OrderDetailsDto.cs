3ebbbdf241d4cc2d3f9ab26f31205d45bc1c24d1
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2025-11-26 20:28:45 +0200

    feat(order): replace string enums with strongly typed enums
    Updated `OrderDetailsProjection`, `OrderDetailsDto`, and related mapping in `OrderRepository` to use strongly typed enums (`OrderDeliveryStatus`, `OrderPaymentOption`, and `OrderPaymentStatus`) instead of string representations. This enhancement improves type safety, reduces potential parsing errors, and ensures consistency across the domain and application layers.
    
    No backward compatibility issues expected as the change is internal and does not affect API contracts.

--- Wolfix.Server/Order.Application/Dto/Order/Responses/OrderDetailsDto.cs
+++ Wolfix.Server/Order.Application/Dto/Order/Responses/OrderDetailsDto.cs
@@ -1,6 +1,8 @@
+using Order.Domain.OrderAggregate.Enums;
+
 namespace Order.Application.Dto.Order.Responses;
 
 public sealed record OrderDetailsDto(Guid Id, string Number, string RecipientFirstName, string RecipientLastName,
-    string RecipientMiddleName, string RecipientPhoneNumber, string DeliveryStatus, string PaymentOption, string PaymentStatus,
-    uint? DeliveryNumber, string DeliveryCity, string DeliveryStreet, uint DeliveryHouseNumber, string DeliveryMethodName,
-    decimal Price, IReadOnlyCollection<OrderItemDetailsDto> OrderItems);
+    string RecipientMiddleName, string RecipientPhoneNumber, OrderDeliveryStatus DeliveryStatus, OrderPaymentOption PaymentOption,
+    OrderPaymentStatus PaymentStatus, uint? DeliveryNumber, string DeliveryCity, string DeliveryStreet, uint DeliveryHouseNumber,
+    string DeliveryMethodName, decimal Price, IReadOnlyCollection<OrderItemDetailsDto> OrderItems);
--- Wolfix.Server/Order.Domain/Projections/OrderDetailsProjection.cs
+++ Wolfix.Server/Order.Domain/Projections/OrderDetailsProjection.cs
@@ -1,7 +1,9 @@
+using Order.Domain.OrderAggregate.Enums;
+
 namespace Order.Domain.Projections;
 
 public sealed record OrderDetailsProjection(Guid Id, string Number, string RecipientFirstName, string RecipientLastName,
-    string RecipientMiddleName, string RecipientPhoneNumber, string DeliveryStatus, string PaymentOption, string PaymentStatus,
-    uint? DeliveryNumber, string DeliveryCity, string DeliveryStreet, uint DeliveryHouseNumber, string DeliveryMethodName,
-    decimal Price, IReadOnlyCollection<OrderItemDetailsProjection> OrderItems);
+    string RecipientMiddleName, string RecipientPhoneNumber, OrderDeliveryStatus DeliveryStatus, OrderPaymentOption PaymentOption,
+    OrderPaymentStatus PaymentStatus, uint? DeliveryNumber, string DeliveryCity, string DeliveryStreet, uint DeliveryHouseNumber,
+    string DeliveryMethodName, decimal Price, IReadOnlyCollection<OrderItemDetailsProjection> OrderItems);
     
