0941bdb4e000520de9ec678e5e149fa1e19a5385
Illia <144327680+ilyaghrischenko@users.noreply.github.com>
2026-01-04 19:06:05 +0200

    feat(support): update support request categories for better clarity
    Updated `SupportRequestCategory` enum by renaming `TechSupport` to `BugOrError` and removing unused categories (`GeneralQuestion` and `Refund`). This change improves the naming accuracy and trims unused options for a more concise and meaningful category list.

--- Wolfix.Server/Support.Domain/Enums/SupportRequestCategory.cs
+++ Wolfix.Server/Support.Domain/Enums/SupportRequestCategory.cs
@@ -2,9 +2,7 @@ namespace Support.Domain.Enums;
 
 public enum SupportRequestCategory
 {
-    GeneralQuestion, // General question
     OrderIssue,      // Order issue
-    TechSupport,     // Site error
-    Refund,           // Refund
+    BugOrError,     // Site error
     Other            // Other
 }
