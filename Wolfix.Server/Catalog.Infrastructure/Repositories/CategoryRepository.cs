5af03b95deca9e5665ef434eba55b1494e6944e1
Sannido <dimabutnik71@gmail.com>
2026-01-16 17:21:27 +0200

    ## Summary Fix parent category filter logic in `CategoryRepository` to improve accuracy
    * Updated `.Where` clause to filter parent categories by `Parent == null` instead of `IsParent`, ensuring correct identification of top-level categories.

--- Wolfix.Server/Catalog.Infrastructure/Repositories/CategoryRepository.cs
+++ Wolfix.Server/Catalog.Infrastructure/Repositories/CategoryRepository.cs
@@ -35,7 +35,7 @@ internal sealed class CategoryRepository(CatalogContext context) :
         List<CategoryFullProjection> parentCategories = await _categories
             .Include(c => c.Parent)
             .AsNoTracking()
-            .Where(category => category.IsParent)
+            .Where(category => category.Parent == null)
             .Select(category => new CategoryFullProjection(category.Id, category.Name, category.PhotoUrl))
             .ToListAsync(ct);
         
